a051861d838896e0339644f1f6926c7145755829
nikdelvin <erter.live@gmail.com>
2024-11-21 12:52:04 +0400

    Domain update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://feelicy.nikdelvin.com',
+    site: 'https://feelicy.by.nikdelv.in',
     integrations: [tailwind()]
 })
